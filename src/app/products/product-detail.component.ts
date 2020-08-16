169d3bad76c8e9ce3bbd4d69864a595ae8d98074
DESKTOP-JH64IUN\erayi <erayizgin@gmail.com>
2020-08-16 21:07:01 +0300

    routing and product detail page

---
+++ src/app/products/product-detail.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'pm-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+  pageTitle = 'Product Detail';
+  product: IProduct;
+  constructor(private route: ActivatedRoute,
+              private router: Router) { }
+
+  ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      'productId': id,
+      'productName': 'Leaf Rake',
+      'productCode': 'GDN-011',
+      'releaseDate': 'March 19, 2019',
+      'description': 'leaf rake',
+      'price': 19.95,
+      'starRating': 3.2,
+      'imageUrl': 'assets/images/leaf_rake.png'
+    }
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
+}
